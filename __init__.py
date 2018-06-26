d7a17ee575268bca33608b4a2cd7885efc66ef67
Alien@lv426 <goupilpierre@gmail.com>
2018-06-26 20:48:53 +0200

    fixed access to Clementine

--- __init__.py
+++ __init__.py
@@ -30,65 +30,64 @@ LOGGER = getLogger(__name__)
 
 class ClementinePlayerSkill(MycroftSkill):
 
-  def __init__(self):
-    super(ClementinePlayerSkill, self).__init__(name="ClementinePlayerSkill")
+    def __init__(self):
+        super(ClementinePlayerSkill, self).__init__(name="ClementinePlayerSkill")
+        self.player = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2')
 
-    self.player = dbus.SessionBus().get_object('org.mpris.MediaPlayer2.clementine', '/org/mpris/MediaPlayer2')
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
 
-  def initialize(self):
-    self.load_data_files(dirname(__file__))
+        play_intent = IntentBuilder("ClementinePlayerPlayIntent").require("Play").build()
+        self.register_intent(play_intent, self.handle_play_intent)
 
-    play_intent = IntentBuilder("ClementinePlayerPlayIntent").require("Play").build()
-    self.register_intent(play_intent, self.handle_play_intent)
+        pause_intent = IntentBuilder("ClementinePlayerPauseIntent").require("Pause").build()
+        self.register_intent(pause_intent, self.handle_pause_intent)
 
-    pause_intent = IntentBuilder("ClementinePlayerPauseIntent").require("Pause").build()
-    self.register_intent(pause_intent, self.handle_pause_intent)
+        stop_intent = IntentBuilder("ClementinePlayerStopIntent").require("Stop").build()
+        self.register_intent(stop_intent, self.handle_stop_intent)
 
-    stop_intent = IntentBuilder("ClementinePlayerStopIntent").require("Stop").build()
-    self.register_intent(stop_intent, self.handle_stop_intent)
+        previous_intent = IntentBuilder("ClementinePlayerPreviousIntent").require("Previous").build()
+        self.register_intent(previous_intent, self.handle_previous_intent)
 
-    previous_intent = IntentBuilder("ClementinePlayerPreviousIntent").require("Previous").build()
-    self.register_intent(previous_intent, self.handle_previous_intent)
+        next_intent = IntentBuilder("ClementinePlayerNextIntent").require("Next").build()
+        self.register_intent(next_intent, self.handle_next_intent)
 
-    next_intent = IntentBuilder("ClementinePlayerNextIntent").require("Next").build()
-    self.register_intent(next_intent, self.handle_next_intent)
+        vol_plus_intent = IntentBuilder("ClementinePlayerVolPlusIntent").require("VolPlus").build()
+        self.register_intent(vol_plus_intent, self.handle_vol_plus_intent)
 
-    vol_plus_intent = IntentBuilder("ClementinePlayerVolPlusIntent").require("VolPlus").build()
-    self.register_intent(vol_plus_intent, self.handle_vol_plus_intent)
+        vol_minus_intent = IntentBuilder("ClementinePlayerVolMinusIntent").require("VolMinus").build()
+        self.register_intent(vol_minus_intent, self.handle_vol_minus_intent)
 
-    vol_minus_intent = IntentBuilder("ClementinePlayerVolMinusIntent").require("VolMinus").build()
-    self.register_intent(vol_minus_intent, self.handle_vol_minus_intent)
+    def handle_play_intent(self):
+        self.player.Play()
 
-  def handle_play_intent(self):
-    self.player.Play()
+    def handle_pause_intent(self):
+        self.player.Pause()
 
-  def handle_pause_intent(self):
-    self.player.Pause()
+    def handle_stop_intent(self):
+        self.player.Stop()
 
-  def handle_stop_intent(self):
-    self.player.Stop()
+    def handle_previous_intent(self):
+        self.player.Prev()
 
-  def handle_previous_intent(self):
-    self.player.Prev()
+    def handle_next_intent(self):
+        self.player.Next()
 
-  def handle_next_intent(self):
-    self.player.Next()
+    def handle_vol_plus_intent(self):
+        volume = self.player.Get('org.mpris.MediaPlayer2.Player', 'Volume',
+                           dbus_interface='org.freedesktop.DBus.Properties')
+        property_interface = dbus.Interface(self.player, dbus_interface='org.freedesktop.DBus.Properties')
+        property_interface.Set('org.mpris.MediaPlayer2.Player', 'Volume', volume + 0.2)
 
-  def handle_vol_plus_intent(self):
-    volume = self.player.Get('org.mpris.MediaPlayer2.Player', 'Volume',
-                             dbus_interface='org.freedesktop.DBus.Properties')
-    property_interface = dbus.Interface(self.player, dbus_interface='org.freedesktop.DBus.Properties')
-    property_interface.Set('org.mpris.MediaPlayer2.Player', 'Volume', volume + 0.2)
+    def handle_vol_minus_intent(self):
+        volume = self.player.Get('org.mpris.MediaPlayer2.Player', 'Volume',
+                           dbus_interface='org.freedesktop.DBus.Properties')
+        property_interface = dbus.Interface(self.player, dbus_interface='org.freedesktop.DBus.Properties')
+        property_interface.Set('org.mpris.MediaPlayer2.Player', 'Volume', volume - 0.2)
 
-  def handle_vol_minus_intent(self):
-    volume = self.player.Get('org.mpris.MediaPlayer2.Player', 'Volume',
-                             dbus_interface='org.freedesktop.DBus.Properties')
-    property_interface = dbus.Interface(self.player, dbus_interface='org.freedesktop.DBus.Properties')
-    property_interface.Set('org.mpris.MediaPlayer2.Player', 'Volume', volume - 0.2)
-
-  def stop(self):
-    pass
+    def stop(self):
+        pass
 
 
 def create_skill():
-  return ClementinePlayerSkill()
+    return ClementinePlayerSkill()
